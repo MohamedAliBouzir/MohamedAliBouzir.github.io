322676da614407689b36282a09fc4c3979d4556e
Ons-bm <onesbenmabrouk@gmail.com>
2022-10-27 23:00:12 +0200

    Ajoutez des fichiers projet.

---
+++ Fermli_Backend/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Fermli_Backend.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fermli_Backend
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentityCore<User>(q => q.User.RequireUniqueEmail = true);
+            builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), services);
+        }
+    }
+}
