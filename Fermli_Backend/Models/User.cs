0a5084b5c3d6d930387d314a8b8dbe8772fdfe62
Ons-bm <onesbenmabrouk@gmail.com>
2022-10-27 23:00:12 +0200

    Ajoutez des fichiers projet.

---
+++ Fermli_Backend/Models/User.cs
@@ -0,0 +1,28 @@
+﻿//using Microsoft.AspNetCore.Identity;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fermli_Backend.Models
+{
+    public class User 
+    {
+
+        public string FullName { get; set; }
+
+        //[Required]
+        //[DataType(DataType.EmailAddress)]
+        //public string Email { get; set; }
+
+        //[Required]
+        //[DataType(DataType.PhoneNumber)]
+        //public string PhoneNumber { get; set; }
+
+        //[Required]
+        
+        //public string Password { get; set; }
+    }
+}
